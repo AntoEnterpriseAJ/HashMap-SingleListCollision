0a31b10b52db5090df0c9ed462233b02f014d4a5
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-05-03 14:33:23 +0300

    modified HashMap.h

--- HashMap.cpp
+++ HashMap.cpp
@@ -2,7 +2,23 @@
 
 template<typename keyType, typename valType, typename hashFunc>
 HashMap<keyType, valType, hashFunc>::HashMap(int size)
-	: m_pos{ size, nullptr }, m_list{}, m_elementsCount{0}, m_size{size}, m_loadFactor{0}
-{}
+	: m_table{ 10, nullptr }, m_list{}, m_size{ size }, m_elementsCount{ 0 }, m_loadFactor{ 0 }
+{
+	m_list.push_back({ -1, -1 }); //dummy node
+}
 
+template<typename keyType, typename valType, typename hashFunc>
+void HashMap<keyType, valType, hashFunc>::insert(const pair& p)
+{
+	std::size_t index = hashFunc(p.first) % m_size;
 
+	if (m_table[index] == nullptr)
+	{
+		m_list.push_back(p);
+		m_table[index] = m_list.begin() - 2;
+	}
+	else
+	{
+		m_list.insert(m_table[index], pair);
+	}
+}
--- HashMap.h
+++ HashMap.h
@@ -7,16 +7,17 @@
 template <typename keyType, typename valType, typename hashFunc = std::hash<keyType>>
 class HashMap
 {
-	uning pair = std::pair<keyType, valType>;
+	using pair = std::pair<keyType, valType>;
+	using listPointer = std::list<pair>::iterator;
 
-	std::vector<std::list<pair>::iterator> m_pos;
+	std::vector<listPointer> m_table;
 	std::list<pair> m_list;
 	int m_elementsCount;
 	int m_size;
 	double m_loadFactor;
 public:
 	HashMap(int size = 10);
-	void insert(pair p);
+	void insert(const pair& p);
 	bool find(const keyType& key);
 	void erase(const keyType& key);
 };
